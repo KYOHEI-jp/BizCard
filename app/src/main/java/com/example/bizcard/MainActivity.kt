03fa40215013a9e99320d519eee64a35551739be
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-03 10:16:26 +0900

    アイコン画面をtop、centerに

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.bizcard
 
 import android.graphics.Color
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,9 +16,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bizcard.ui.theme.BizCardTheme
@@ -48,15 +56,35 @@ fun CreateBizCard() {
                 .width(200.dp)
                 .height(390.dp)
                 .padding(12.dp),
-            elevation = 4.dp,
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            backgroundColor = White
+            elevation = 4.dp
         ) {
+            Column(
+                modifier = Modifier.height(300.dp), verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(5.dp),
+                    shape = CircleShape,
+                    border = BorderStroke(0.5.dp, LightGray),
+                    elevation = 4.dp,
+                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
 
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.profile_image),
+                        contentDescription = "profile image",
+                        modifier = Modifier.size(135.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
         }
+
     }
 }
-
 
 @Composable
 fun Greeting(name: String) {
